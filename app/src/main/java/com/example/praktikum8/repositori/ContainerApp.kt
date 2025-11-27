2765714026bfb44f2e459431af9e142ec8492e72
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-11-27 16:28:31 +0700

    menambah container app yang offline repositori siswa

---
+++ app/src/main/java/com/example/praktikum8/repositori/ContainerApp.kt
@@ -0,0 +1,17 @@
+package com.example.praktikum8.repositori
+
+import android.content.Context
+import com.example.praktikum8.room.DatabaseSiswa
+
+interface ContainerApp {
+    val repositoriSiswa : RepositoriSiswa
+}
+
+class ContainerDataApp(private val context: Context):
+    ContainerApp {
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa(
+            DatabaseSiswa.getDatabase(context).siswaDao())
+    }
+}
+
