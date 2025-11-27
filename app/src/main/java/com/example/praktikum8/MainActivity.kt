30d058b9039be02e351a9b641c919f9f83872a05
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-11-27 21:01:19 +0700

    memodifaksi main activity

--- app/src/main/java/com/example/praktikum8/MainActivity.kt
+++ app/src/main/java/com/example/praktikum8/MainActivity.kt
@@ -7,41 +7,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.praktikum8.ui.theme.Praktikum8Theme
+import com.example.praktikum8.view.uicontroller.SiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            // Ini konteks Composable, aman memanggil fungsi Composable
             Praktikum8Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    // SiswaApp adalah Composable, jadi bisa dipanggil di sini
+                    SiswaApp(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Praktikum8Theme {
-        Greeting("Android")
     }
 }
