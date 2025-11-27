d5936b483371e249dc186b0a1faa734916a5e1fb
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-11-27 17:04:25 +0700

    menambah data class entry view model

--- app/src/main/java/com/example/praktikum8/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/praktikum8/viewmodel/EntryViewModel.kt
@@ -1,7 +1,43 @@
 package com.example.praktikum8.viewmodel
 
-
 import com.example.praktikum8.room.Siswa
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.praktikum8.repositori.RepositoriSiswa
+
+class EntryViewModel (private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+    /**
+     * Berisi status Siswa saat ini
+     */
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+    /* Fungsi untuk memvalidasi input*/
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(
+                detailSiswa = detailSiswa,
+                isEntryValid = validasiInput(detailSiswa)
+            )
+    }
+
+    suspend fun saveSiswa() {
+        if (validasiInput()) {
+            repositoriSiswa.insertSiswa(
+                uiStateSiswa
+                    .detailSiswa.toSiswa()
+            )
+        }
+    }
+
+}
 
 data class UIStateSiswa(
     val detailSiswa: DetailSiswa = DetailSiswa(),
