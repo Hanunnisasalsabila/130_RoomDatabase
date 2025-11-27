8029911797969b71dc4f58773490cd5b4b55aad1
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-11-27 20:17:12 +0700

    tambahkan DestinasiHome sebagai implementasi DestinasiNavigasi

---
+++ app/src/main/java/com/example/praktikum8/view/route/DestinasiHome.kt
@@ -0,0 +1,9 @@
+package com.example.praktikum8.view.route
+
+import com.example.praktikum8.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+
+    override val titleRes = R.string.app_name
+}
