a0ab7c7009c6542842a27c548d0dd60a8aa7205b
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-12-04 15:45:59 +0700

    memperbaiki penyedia view model

--- app/src/main/java/com/example/praktikum8/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/praktikum8/viewmodel/provider/PenyediaViewModel.kt
@@ -1,21 +1,40 @@
 package com.example.praktikum8.viewmodel.provider
 
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.praktikum8.repositori.AplikasiSiswa
 import com.example.praktikum8.viewmodel.EntryViewModel
 import com.example.praktikum8.viewmodel.HomeViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import com.example.praktikum8.view.DetailDataSiswa
+import com.example.praktikum8.viewmodel.DetailViewModel
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
+
+        // HAPUS BARIS INI: val aplikasiSiswa = this.aplikasiSiswa()
+
         initializer {
-            val application = this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa
-            HomeViewModel(application.container.repositoriSiswa)
+            // Pindah variabel ke sini, di mana 'this' adalah CreationExtras
+            val aplikasiSiswa = this.aplikasiSiswa()
+            HomeViewModel(
+                aplikasiSiswa.container.repositoriSiswa
+            )
         }
         initializer {
-            val application = this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa
-            EntryViewModel(application.container.repositoriSiswa)
+            // Ulangi untuk EntryViewModel
+            val aplikasiSiswa = this.aplikasiSiswa()
+            EntryViewModel(
+                aplikasiSiswa.container.repositoriSiswa
+            )
+        }
+        initializer {
+            DetailViewModel(this.createSavedStateHandle(),
+                aplikasiSiswa().container.repositoriSiswa)
         }
     }
 }
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
