0f28ba2ca4ead4678c651b740960e088c7b6494e
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-12-04 21:16:02 +0700

    tambah fungsi getSiswaStream untuk mengambil data Siswa secara realtime

--- app/src/main/java/com/example/praktikum8/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/praktikum8/repositori/RepositoriSiswa.kt
@@ -8,9 +8,9 @@ interface  RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
     suspend fun insertSiswa(siswa: Siswa)
 
-    fun getSiswaStream(id: Int): Flow<Siswa?>
     suspend fun deleteSiswa(siswa: Siswa)
     suspend fun updateSiswa(siswa: Siswa)
+    fun getSiswaStream(id: Int): Flow<Siswa?>
 }
 
 class OfflineRepositoriSiswa(
@@ -18,7 +18,6 @@ class OfflineRepositoriSiswa(
 ):  RepositoriSiswa {
     override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
-
     override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
     override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
     override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)
